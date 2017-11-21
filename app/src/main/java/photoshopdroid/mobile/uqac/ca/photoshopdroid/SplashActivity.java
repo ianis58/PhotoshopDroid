167640b61d547cdbb686e8dbc847f005050ffa5b
Ianis R <ianis58@gmail.com>
2017-11-21 16:22:47 -0500

    Initial commit

---
+++ app/src/main/java/photoshopdroid/mobile/uqac/ca/photoshopdroid/SplashActivity.java
@@ -0,0 +1,13 @@
+package photoshopdroid.mobile.uqac.ca.photoshopdroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
