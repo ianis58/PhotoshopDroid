162c386fd07e83edc53bed7ee31a47f32e703ff7
Ianis R <ianis58@gmail.com>
2017-11-21 16:43:37 -0500

    add of permission read/write on sdcard request, add of splash activity layout

---
+++ app/src/main/java/photoshopdroid/mobile/uqac/ca/photoshopdroid/DrawActivity.java
@@ -0,0 +1,13 @@
+package photoshopdroid.mobile.uqac.ca.photoshopdroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DrawActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_draw);
+    }
+}
--- app/src/main/java/photoshopdroid/mobile/uqac/ca/photoshopdroid/SplashActivity.java
+++ app/src/main/java/photoshopdroid/mobile/uqac/ca/photoshopdroid/SplashActivity.java
@@ -1,13 +1,50 @@
 package photoshopdroid.mobile.uqac.ca.photoshopdroid;
 
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
+
+    private Button btnNew;
+    private Button btnLoad;
+
+    private static final int REQUEST_WRITE_PERMISSION = 786;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == REQUEST_WRITE_PERMISSION && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            //openFilePicker();
+        }
+    }
+
+    private void requestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_WRITE_PERMISSION);
+        } else {
+            //openFilePicker();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        requestPermission();
+
+        btnNew = (Button) findViewById(R.id.btnNew);
+        btnLoad = (Button) findViewById(R.id.btnLoad);
+
+        btnNew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
