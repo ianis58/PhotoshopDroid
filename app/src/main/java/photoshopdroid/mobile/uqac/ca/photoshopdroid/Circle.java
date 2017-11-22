e7bff07932f2f931d01926cef2b55ba077c4f72a
YannKlemens <yannklemensragueneau@gmail.com>
2017-11-22 00:41:04 -0500

    Added circle and line GeometricObjects, detailed how GeometricObjects must be drawn when selected or unselected

---
+++ app/src/main/java/photoshopdroid/mobile/uqac/ca/photoshopdroid/Circle.java
@@ -0,0 +1,10 @@
+package PACKAGE_NAME;
+
+import photoshopdroid.mobile.uqac.ca.photoshopdroid.GeometricShape;
+
+/**
+ * Created by Utilisateur on 2017-11-21.
+ */
+
+public class Circle extends GeometricShape {
+}
---
+++ app/src/main/java/photoshopdroid/mobile/uqac/ca/photoshopdroid/Line.java
@@ -0,0 +1,7 @@
+import photoshopdroid.mobile.uqac.ca.photoshopdroid.PointsShape;
+
+public class Line extends PointsShape {
+    public Line(int color, int width){
+        super(color, width);
+    }
+}
